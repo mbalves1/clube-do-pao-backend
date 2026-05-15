7824a0f4b54ae09ff2d6bc12053aca11631c5d0c
Murilo <mbalves1@outlook.com>
2026-05-15 08:36:50 -0300

    feat: enhance bakery model and validation with additional address fields

---
+++ prisma/migrations/20260515113424_/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "bakeries" ADD COLUMN     "city" TEXT,
+ADD COLUMN     "district" TEXT,
+ADD COLUMN     "number" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "street" TEXT,
+ADD COLUMN     "zipCode" TEXT;
