aa880dcab8547bf6246ba9f4c2655e1ec676c69e
Murilo <mbalves1@outlook.com>
2026-05-18 08:51:36 -0300

    feat: update subscription creation to support weekly frequency and specific days

--- src/core/usecases/subscribe/create-subscribe.ts
+++ src/core/usecases/subscribe/create-subscribe.ts
@@ -9,6 +9,7 @@ interface CreateSubscribeInput {
 	serviceStartAt: string;
 	serviceEndAt: string;
 	notes: string;
+	daysWeek?: string[];
 }
 
 export class CreateSubscribeUseCase {
@@ -17,6 +18,22 @@ export class CreateSubscribeUseCase {
 		private bakeryRepository: BakeryRepository,
 		private subscribeRepository: SubscribeRepository,
 	) {}
+
+	private weekMap: Record<string, number> = {
+		sunday: 0,
+		monday: 1,
+		tuesday: 2,
+		wednesday: 3,
+		thursday: 4,
+		friday: 5,
+		saturday: 6,
+	};
+
+	private parseDate(date: string): Date {
+		const [day, month, year] = date.split('-').map(Number);
+
+		return new Date(year, month - 1, day);
+	}
 
 	async execute(
 		idUser: string,
@@ -51,19 +68,39 @@ export class CreateSubscribeUseCase {
 					notes: subscribe.notes,
 				});
 			}
-		} else {
-			return this.subscribeRepository.create({
-				userId: idUser,
-				bakeryId: idBakery,
-				serviceDate: new Date(),
-				serviceStartAt: subscribe.deliveryStartAt,
-				serviceEndAt: subscribe.deliveryEndAt,
-				frequency: subscribe.frequency,
-				deliveryStartAt: subscribe.deliveryStartAt,
-				deliveryEndAt: subscribe.deliveryEndAt,
-				status: 'PENDING',
-				notes: subscribe.notes,
-			});
+		}
+
+		const startDate = this.parseDate(subscribe.serviceStartAt);
+		const endDate = this.parseDate(subscribe.serviceEndAt);
+
+		if (subscribe.frequency === 'weekly' && subscribe.daysWeek?.length) {
+			const selectedDays = subscribe.daysWeek.map(
+				(day: any) => this.weekMap[day.toLowerCase()],
+			);
+
+			const currentDate = new Date(startDate);
+
+			while (currentDate <= endDate) {
+				const currentWeekDay = currentDate.getDay();
+
+				if (selectedDays.includes(currentWeekDay)) {
+					await this.subscribeRepository.create({
+						userId: idUser,
+						bakeryId: idBakery,
+						serviceDate: new Date(currentDate),
+						serviceStartAt: subscribe.serviceStartAt,
+						serviceEndAt: subscribe.serviceEndAt,
+						frequency: subscribe.frequency,
+						deliveryStartAt: subscribe.deliveryStartAt,
+						deliveryEndAt: subscribe.deliveryEndAt,
+						status: 'PENDING',
+						daysWeek: subscribe.daysWeek,
+						notes: subscribe.notes,
+					});
+				}
+
+				currentDate.setDate(currentDate.getDate() + 1);
+			}
 		}
 	}
 }
