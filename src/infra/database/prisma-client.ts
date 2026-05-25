ee382c9d61da3f2a200a2556fb0bfb40e24699ff
Murilo <mbalves1@outlook.com>
2026-05-25 19:22:08 -0300

    fix prisma client on vercel

--- src/infra/database/prisma-client.ts
+++ src/infra/database/prisma-client.ts
@@ -1,5 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient({
-	datasourceUrl: process.env.DATABASE_URL,
+let prismaClient: PrismaClient | null = null;
+
+function getPrismaClient() {
+	if (!prismaClient) {
+		prismaClient = new PrismaClient({
+			datasourceUrl: process.env.DATABASE_URL,
+		});
+	}
+
+	return prismaClient;
+}
+
+export const prisma = new Proxy({} as PrismaClient, {
+	get(_target, prop) {
+		const value = Reflect.get(getPrismaClient(), prop);
+
+		if (typeof value === 'function') {
+			return value.bind(getPrismaClient());
+		}
+
+		return value;
+	},
 });
