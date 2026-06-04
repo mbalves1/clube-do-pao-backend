a195006ec166b9a1d0dd600b6707a6a8e1adac13
Murilo <mbalves1@outlook.com>
2026-06-04 09:15:17 -0300

    [feat]: add tratamento de erro no useCase de subscribe

--- src/core/usecases/subscribe/list-subscribe.ts
+++ src/core/usecases/subscribe/list-subscribe.ts
@@ -1,9 +1,18 @@
+import { NotFoundError } from '../../errors/NotFoundError';
 import { SubscribeRepository } from '../../ports/subscribe-repository';
 
 export class ListSubscribeUseCase {
 	constructor(private subscribeRepository: SubscribeRepository) {}
 
 	async execute(idUser: string): Promise<any> {
-		return this.subscribeRepository.getList(idUser);
+		const subscriptions = await this.subscribeRepository.getList(idUser);
+
+		if (!subscriptions?.length) {
+			throw new NotFoundError(
+				'Nenhuma assinatura encontrada para este usuário',
+			);
+		}
+
+		return subscriptions;
 	}
 }
